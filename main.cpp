e80ef12e80c8aaa6bb864632043ef53ec8278928
RooiGevaar19 <redthreat19@gmail.com>
2020-08-17 17:28:26 +0200

    window event loop

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 
 //The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
+SDL_Surface* gTurtle = NULL;
 
 bool init() {
     bool success = true;
@@ -27,21 +27,21 @@ bool init() {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
             success = false;
         } else {
-            gScreenSurface = SDL_GetWindowSurface( gWindow );
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
         }
     }
 
     return success;
 }
 
-bool loadMedia() {
+bool loadTurtleMedia(char fname[]) {
     //Loading success flag
     bool success = true;
 
     //Load splash image
-    gHelloWorld = SDL_LoadBMP("images/turtley.bmp");
-    if(gHelloWorld == NULL) {
-        printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+    gTurtle = SDL_LoadBMP(fname);
+    if(gTurtle == NULL) {
+        printf( "Unable to load image %s! SDL Error: %s\n", fname, SDL_GetError() );
         success = false;
     }
 
@@ -49,8 +49,8 @@ bool loadMedia() {
 }
 
 void close() {
-    SDL_FreeSurface(gHelloWorld);
-    gHelloWorld = NULL;
+    SDL_FreeSurface(gTurtle);
+    gTurtle = NULL;
 
     SDL_DestroyWindow(gWindow);
     gWindow = NULL;
@@ -63,13 +63,26 @@ int main(int argc, char* args[]) {
     if (!init()) {
         printf("Failed to initialize!\n");
     } else {
-        if (!loadMedia()) {
+        if (!loadTurtleMedia("images/turtley_screen.bmp")) {
             printf("Failed to load media!\n");
         } else {
+            bool quit = false;
+            SDL_Event e;
+
             //Apply the image
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-            SDL_UpdateWindowSurface(gWindow);
             SDL_Delay(2000);
+
+            //While application is running
+            while(!quit) {
+                while( SDL_PollEvent( &e ) != 0 ) {
+                    //User requests quit
+                    if(e.type == SDL_QUIT) {
+                        quit = true;
+                    }
+                }
+                SDL_BlitSurface(gTurtle, NULL, gScreenSurface, NULL);
+                SDL_UpdateWindowSurface(gWindow);
+            }
         }
     }
 
